a309d1252fedcdbbc39fd423765d7cc2ca8a872f
iejerry <546842522@qq.com>
2013-11-26 10:28:18 +0800

    update

--- src/jquery-scrollToTop.js
+++ src/jquery-scrollToTop.js
@@ -16,9 +16,11 @@
 
 		this.namespace = this.options.namespace;
 		this.easing = 'easing_' + this.options.easing;
+		if (this.options.skin === null) {
+			this.options.skin = 'default';
+		}
 		this.skin = this.namespace + '_' + this.options.skin;
 		this.disabled = false;
-		this.sheet = document.styleSheets[document.styleSheets.length - 1];
 
 		var self = this;
 		$.extend(self, {
@@ -35,51 +37,51 @@
 					if (self.disabled) {
 						return;
 					}
-					if (!$('.' + self.namespace + '_' + self.options.animation).css('animation-duration')) {
-						$('body, html').animate({
+					if (self.transition.supported) {
+						var pos = $(window).scrollTop();
+						self.$doc.css({
+							'margin-top': -pos + 'px'
+						});
+						$(window).scrollTop(0);
+						self.insertRule('.' + self.easing + '{' + self.transition.prefix + 'transition-duration: ' + self.options.speed + 'ms;}');
+						self.$doc.addClass(self.easing).css({
+							'margin-top': ''
+						});
+					} else {
+						$('body, html').stop(true, false).animate({
 							scrollTop: 0
 						}, self.options.speed);
 						return;
 					}
-					var pos = $(window).scrollTop();
-					self.$doc.css({
-						'margin-top': -pos + 'px'
+				})
+					.on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function() {
+						self.$doc.removeClass(self.easing);
+					})
+					.on('ScrollToTop::show', function() {
+						self.$doc.addClass(self.namespace + '_show');
+
+						self.$trigger.addClass(self.namespace + '_' + self.options.animation);
+					})
+					.on('ScrollToTop::hide', function() {
+						self.$doc.removeClass(self.namespace + '_show');
+
+						self.$trigger.removeClass(self.namespace + '_' + self.options.animation);
+					})
+					.on('ScrollToTop::disable', function() {
+						self.disabled = true;
+						self.$doc.trigger('ScrollToTop::hide');
+					})
+					.on('ScrollToTop::enable', function() {
+						self.disabled = false;
+
+						self.toggle();
 					});
-
-					$(window).scrollTop(0);
-					self.insertRule(self.sheet, '.' + self.easing, '-webkit-transition-duration: ' + self.options.speed + 'ms; transition-duration: ' + self.options.speed + 'ms;', 0);
-					self.$doc.addClass(self.easing).css({
-						'margin-top': ''
-					});
-				})
-				.on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function() {
-					self.$doc.removeClass(self.easing);
-				})
-				.on('ScrollToTop::show', function() {
-					self.$doc.addClass(self.namespace + '_show');
-
-					self.$trigger.addClass(self.namespace + '_' + self.options.animation);
-				})
-				.on('ScrollToTop::hide', function() {
-					self.$doc.removeClass(self.namespace + '_show');
-
-					self.$trigger.removeClass(self.namespace + '_' + self.options.animation);
-				})
-				.on('ScrollToTop::disable', function() {
-					self.disabled = true;
-					self.$doc.trigger('ScrollToTop::hide');
-				})
-				.on('ScrollToTop::enable', function() {
-					self.disabled = false;
-
-					self.toggle();
-				});
 
 				self.toggle();
 			},
 			build: function() {
 				self.$trigger = $('<a href="#" id="' + self.namespace + '" class="' + self.namespace + ' ' + self.skin + '">' + self.options.text + '</a>').appendTo($('body'));
-				self.insertRule(self.sheet, '.' + self.namespace + '_' + self.options.animation, '-webkit-animation-duration: ' + self.options.animationSpeed + 'ms; animation-duration: ' + self.options.animationSpeed + 'ms;', 0);
+				self.insertRule('.' + self.namespace + '_' + self.options.animation + '{' + self.transition.prefix + 'animation-duration: ' + self.options.animationSpeed + 'ms;}');
 			},
 			can: function() {
 				if ($(window).scrollTop() > self.options.distance) {
@@ -95,11 +97,49 @@
 					self.$doc.trigger('ScrollToTop::hide');
 				}
 			},
-			insertRule: function(sheet, selectorText, cssText, position) {
-				if (sheet.insertRule) {
-					sheet.insertRule(selectorText + "{" + cssText + "}", position);
-				} else if (sheet.addRule) {
-					sheet.addRule(selectorText, cssText, position);
+			transition: function() {
+				var e,
+					end,
+					prefix = '',
+					supported = false,
+					el = document.createElement("fakeelement"),
+					transitions = {
+						"WebkitTransition": "webkitTransitionEnd",
+						"MozTransition": "transitionend",
+						"OTransition": "oTransitionend",
+						"transition": "transitionend"
+					};
+				for (e in transitions) {
+					if (el.style[e] !== undefined) {
+						end = transitions[e];
+						supported = true;
+						break;
+					}
+				}
+				if (/(WebKit)/i.test(window.navigator.userAgent)) {
+					prefix = '-webkit-';
+				}
+				return {
+					prefix: prefix,
+					end: end,
+					supported: supported
+				};
+			},
+			insertRule: function(rule) {
+				if (self.rules && self.rules[rule]) {
+					return;
+				} else if (self.rules === undefined) {
+					self.rules = {};
+				} else {
+					self.rules[rule] = true;
+				}
+
+				if (document.styleSheets && document.styleSheets.length) {
+					document.styleSheets[0].insertRule(rule, 0);
+				} else {
+					var style = document.createElement('style');
+					style.innerHTML = rule;
+					document.head.appendChild(style);
 				}
 			}
 		});
@@ -140,11 +180,11 @@
 		destroy: function() {
 			this.$trigger.remove();
 			this.$doc.data('ScrollToTop', null);
-			this.$doc.off('ScrollToTop::enable');
-			this.$doc.off('ScrollToTop::disable');
-			this.$doc.off('ScrollToTop::jump');
-			this.$doc.off('ScrollToTop::show');
-			this.$doc.off('ScrollToTop::hide');
+			this.$doc.off('ScrollToTop::enable')
+				.off('ScrollToTop::disable')
+				.off('ScrollToTop::jump')
+				.off('ScrollToTop::show')
+				.off('ScrollToTop::hide');
 		}
 	};
 
